814df703d308c0c4612e3cc9e76c809c9d16dea5
drval <rudrval@gmail.com>
2018-05-26 18:11:11 +0400

    отклонение от среднего < 1*sd

--- list.R
+++ list.R
@@ -5,3 +5,9 @@ ls <- list(age, is_married, name)
 ls[[1]][1]
 df <- data.frame(Name = name, Age = age, Status = is_married)
 typeof(df)
+
+#****************************
+v <- c(10, 20, 30, 6, 45, 11, 54, 87, 33)
+v[abs(v - mean(v)) < sd(v)]
+mean(v)
+sd(v)
