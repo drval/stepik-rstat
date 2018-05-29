882b25eb10ad5462da19bb1dc81dd502da7b4962
drval <rudrval@gmail.com>
2018-05-30 02:51:08 +0400

    assign

--- nominative_an.R
+++ nominative_an.R
@@ -14,4 +14,12 @@ prop.table(t2, 1)
 prop.table(t2, 2)
 
 t3 <- table(Years= df$years_in_uni, field=df$field,status=df$status )
+###############
+dimnames(HairEyeColor)
+# доля рыжеволосых (Red) от общего числа голубоглазых мужчин.
+#v1
+as.numeric( prop.table(HairEyeColor[,'Blue','Male'])['Red'])
+#v2.
+prop.table(HairEyeColor[ , ,'Male'],2)['Red','Blue']
 
+sum(HairEyeColor[,'Green', 'Female'])
