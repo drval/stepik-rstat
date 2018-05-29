e84584d3fb945075d6a89f3975873d89f24d8138
drval <rudrval@gmail.com>
2018-05-30 02:33:22 +0400

    table, prop

--- nominative_an.R
+++ nominative_an.R
@@ -1,2 +1,17 @@
-grants <- read.csv('https://stepic.org/media/attachments/lesson/11502/grants.csv')
+df <- read.csv('https://stepic.org/media/attachments/lesson/11502/grants.csv')
+df$status <- as.factor(df$status)
+levels(df$status) <- c('Not funded', 'funded')
+#v.2
+df$status <- factor(df$status, labels = c('Not funded', 'funded'))
+
+t1 <- table(df$status)
+dim(t1)
+t2 <- table(df$status, df$field)
+t2
+t2 <- table(status=df$status, field=df$field)
+prop.table(t2)
+prop.table(t2, 1)
+prop.table(t2, 2)
+
+t3 <- table(Years= df$years_in_uni, field=df$field,status=df$status )
 
