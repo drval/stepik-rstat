743179c5767c05ffbe8362c7649f3d792b6a8cac
drval <rudrval@gmail.com>
2018-11-01 23:43:12 +0400

    -

--- opt-model.R
+++ opt-model.R
@@ -65,3 +65,74 @@ normality.test  <- function(x){
   unlist(lapply(x, function(v) shapiro.test(v)$p.value ))
 }
 normality.test(iris[,-5])
+#############################################################
+library(ggplot2)
+ggplot(swiss, aes(Examination, Education))+
+  geom_point()+geom_smooth()
+
+lm1 <- lm(Education ~ Examination, swiss)
+summary(lm1)
+swiss$Examination_squared <-  (swiss$Examination)^2
+lm2 <- lm(Education ~ Examination + Examination_squared, swiss)
+summary(lm2)
+ 
+anova(lm2, lm1)
+
+swiss$lm1_fitted <- lm1$fitted.values
+swiss$lm2_fitted <- lm2$fitted.values
+
+ggplot(swiss, aes(x = Examination, y = Education))+
+  geom_point(size=3)+
+  geom_line(aes(x =Examination, y=lm1_fitted), col='red', lwd=1)+
+  geom_line(aes(x =Examination, y=lm2_fitted), col='blue', lwd=1)
+
+swiss$lm1_resid <- lm1$resid
+swiss$lm2_resid <- lm2$resid
+
+ggplot(swiss, aes(x=lm1_fitted, y=lm1_resid))+
+  geom_point(size=1)+ 
+  geom_hline(yintercept =0, col='red', lwd=1)
+
+ggplot(swiss, aes(x=lm2_fitted, y=lm2_resid))+
+  geom_point(size=1)+ 
+  geom_hline(yintercept =0, col='red', lwd=1)
+
+swiss$obs_number <- 1:nrow(swiss)
+ggplot(swiss, aes(x = obs_number, y = lm1_resid))+
+  geom_point(size=3)+geom_smooth()
+
+ggplot(swiss, aes(x = obs_number, y = lm2_resid))+
+  geom_point(size=3)+geom_smooth()
+
+
+ggplot(swiss, aes(x=lm1_fitted, y=lm1_resid))+
+  geom_point(size=3)
+
+ggplot(swiss, aes(x=lm2_fitted, y=lm2_resid))+
+  geom_point(size=3)
+
+
+ds <- read.csv('https://stepic.org/media/attachments/lesson/12088/homosc.csv')
+install.packages('gvlma')
+library(gvlma)
+xds <- gvlma(DV~IV, ds)
+summary(xds)
+
+ggplot(swiss, aes(x=lm1_resid))+
+  geom_histogram(binwidth = 4, fill='white', col='black')
+
+qqnorm(lm1$residuals)
+qqline(lm1$residuals)
+
+shapiro.test(lm1$residuals)
+
+ggplot(swiss, aes(x=lm2_resid))+
+  geom_histogram(binwidth = 4, fill='white', col='black')
+
+qqnorm(lm2$residuals)
+qqline(lm2$residuals)
+
+shapiro.test(lm2$residuals)
+
+
+#############################################################
